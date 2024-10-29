59a46ee2f752639c24fdb695de1117d5f6b32fd2
TranCongViet <trancongviet990ntt@gmail.com>
2024-10-29 23:29:38 +0700

    add file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/frontend_user_register/",
   plugins: [react()],
   server: {
     port: 5173, // Thay đổi port tại đây
